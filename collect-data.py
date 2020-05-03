92238619d1cda583c19a5a1d4fbaacb558e1b50c
nivedhashanmugam679 <35552026+nivedhashanmugam679@users.noreply.github.com>
2020-05-04 05:09:40 +0530

    Update collect-data.py

--- collect-data.py
+++ collect-data.py
@@ -13,30 +13,30 @@ if not os.path.exists("data"):
     os.makedirs("data/train/3")
     os.makedirs("data/train/4")
     os.makedirs("data/train/5")
-    os.makedirs("data/train/A1")
-    os.makedirs("data/train/C1")
-    os.makedirs("data/train/D1")
-    os.makedirs("data/train/H1")
-    os.makedirs("data/train/L1")
-    os.makedirs("data/train/M1")
-    os.makedirs("data/train/P1")
-    os.makedirs("data/train/R1")
-    os.makedirs("data/train/U1")
+    os.makedirs("data/train/A")
+    os.makedirs("data/train/C")
+    os.makedirs("data/train/D")
+    os.makedirs("data/train/H")
+    os.makedirs("data/train/L")
+    os.makedirs("data/train/M")
+    os.makedirs("data/train/P")
+    os.makedirs("data/train/R")
+    os.makedirs("data/train/U")
     os.makedirs("data/test/0")
     os.makedirs("data/test/1")
     os.makedirs("data/test/2")
     os.makedirs("data/test/3")
     os.makedirs("data/test/4")
     os.makedirs("data/test/5")
-    os.makedirs("data/test/A1")
-    os.makedirs("data/test/C1")
-    os.makedirs("data/test/D1")
-    os.makedirs("data/test/H1")
-    os.makedirs("data/test/L1")
-    os.makedirs("data/test/M1")
-    os.makedirs("data/test/P1")
-    os.makedirs("data/test/R1")
-    os.makedirs("data/test/U1")
+    os.makedirs("data/test/A")
+    os.makedirs("data/test/C")
+    os.makedirs("data/test/D")
+    os.makedirs("data/test/H")
+    os.makedirs("data/test/L")
+    os.makedirs("data/test/M")
+    os.makedirs("data/test/P")
+    os.makedirs("data/test/R")
+    os.makedirs("data/test/U")
  
 # Train or test 
 mode = 'train'
@@ -56,15 +56,15 @@ while True:
              'three': len(os.listdir(directory+"/3")),
              'four': len(os.listdir(directory+"/4")),
              'five': len(os.listdir(directory+"/5"))}
-             'a': len(os.listdir(directory+"/A1"))}
-             'c': len(os.listdir(directory+"/C1"))}
-             'd': len(os.listdir(directory+"/D1"))}
-             'h': len(os.listdir(directory+"/H1"))}
-             'l': len(os.listdir(directory+"/L1"))}
-             'm': len(os.listdir(directory+"/M1"))}
-             'p': len(os.listdir(directory+"/P1"))}
-             'r': len(os.listdir(directory+"/R1"))}
-             'u': len(os.listdir(directory+"/U1"))}
+             'a': len(os.listdir(directory+"/A"))}
+             'c': len(os.listdir(directory+"/C"))}
+             'd': len(os.listdir(directory+"/D"))}
+             'h': len(os.listdir(directory+"/H"))}
+             'l': len(os.listdir(directory+"/L"))}
+             'm': len(os.listdir(directory+"/M"))}
+             'p': len(os.listdir(directory+"/P"))}
+             'r': len(os.listdir(directory+"/R"))}
+             'u': len(os.listdir(directory+"/U"))}
     
     # Printing the count in each set to the screen
     cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
@@ -75,15 +75,15 @@ while True:
     cv2.putText(frame, "THREE : "+str(count['three']), (10, 180), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     cv2.putText(frame, "FOUR : "+str(count['four']), (10, 200), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     cv2.putText(frame, "FIVE : "+str(count['five']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "A : "+str(count['a']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "C : "+str(count['c']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "D : "+str(count['d']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "H : "+str(count['h']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "L : "+str(count['l']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "M : "+str(count['m']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "P : "+str(count['p']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "R : "+str(count['r']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    cv2.putText(frame, "U : "+str(count['u']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "A_ASL : "+str(count['a']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "C_ASL : "+str(count['c']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "D_ASL : "+str(count['d']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "H_ASL : "+str(count['h']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "L_ASL : "+str(count['l']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "M_ASL : "+str(count['m']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "P_ASL : "+str(count['p']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "R_ASL : "+str(count['r']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "U_ASL : "+str(count['u']), (10, 220), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     
     # Coordinates of the ROI
     x1 = int(0.5*frame.shape[1])
@@ -123,24 +123,24 @@ while True:
         cv2.imwrite(directory+'4/'+str(count['four'])+'.jpg', roi)
     if interrupt & 0xFF == ord('5'):
         cv2.imwrite(directory+'5/'+str(count['five'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('A1'):
-        cv2.imwrite(directory+'A1/'+str(count['a'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('C1'):
-        cv2.imwrite(directory+'C1/'+str(count['c'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('D1'):
-        cv2.imwrite(directory+'D1/'+str(count['d'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('H1'):
-        cv2.imwrite(directory+'H1/'+str(count['h'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('L1'):
-        cv2.imwrite(directory+'L1/'+str(count['l'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('M1'):
-        cv2.imwrite(directory+'M1/'+str(count['m'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('P1'):
-        cv2.imwrite(directory+'P1/'+str(count['p'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('R1'):
-        cv2.imwrite(directory+'R1/'+str(count['r'])+'.jpg', roi)
-    if interrupt & 0xFF == ord('U1'):
-        cv2.imwrite(directory+'U1/'+str(count['u'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('A'):
+        cv2.imwrite(directory+'A/'+str(count['a'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('C'):
+        cv2.imwrite(directory+'C/'+str(count['c'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('D'):
+        cv2.imwrite(directory+'D/'+str(count['d'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('H'):
+        cv2.imwrite(directory+'H/'+str(count['h'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('L'):
+        cv2.imwrite(directory+'L/'+str(count['l'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('M'):
+        cv2.imwrite(directory+'M/'+str(count['m'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('P'):
+        cv2.imwrite(directory+'P/'+str(count['p'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('R'):
+        cv2.imwrite(directory+'R/'+str(count['r'])+'.jpg', roi)
+    if interrupt & 0xFF == ord('U'):
+        cv2.imwrite(directory+'U/'+str(count['u'])+'.jpg', roi)
     
 cap.release()
 cv2.destroyAllWindows()
